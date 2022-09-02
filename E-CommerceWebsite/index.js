a8e771aa38bb310a7fcd384ee2a12d4a539359a2
Charlotte Lokko <32336323+charlottelokko@users.noreply.github.com>
2022-09-02 18:47:39 +0100

    Delete E-CommerceWebsite directory

--- E-CommerceWebsite/index.js
+++
@@ -1,8 +0,0 @@
-function openMenu(){
-    //adding a class to the body to let know that menu is open
-    document.body.classList += " menu--open";  //classList can be a huge string of classes
-}
-
-function closeMenu(){
-    document.body.classList.remove('menu--open');
-}
